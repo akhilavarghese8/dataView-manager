64723e1bfb07d5f9596805f6c3c9095c070ddb0a
Aradhyansasi <aradhya.n.sasi101@gmail.com>
2024-12-23 15:44:29 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { CommonLayoutComponent } from './components/common-layout/common-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: CommonLayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "employee",
+        pathMatch: "full"
+      },
+      {
+        path: 'employee',
+        loadChildren: () => 
+          import('./components/employee/employee.module').then(
+            m => m.EmployeeModule
+          )
+      },
+      {
+        path: 'apidatatable',
+        loadChildren: () => 
+          import('./components/api-data-table/api-data-table.module').then(
+            m => m.APIDataTableModule
+          )
+      },
+      {
+        path:'**',
+        pathMatch:'full',
+        component:ErrorPageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APIDataTableComponent } from './components/api-data-table/api-data-table.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonLayoutComponent } from './components/common-layout/common-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    APIDataTableComponent,
+    ErrorPageComponent,
+    CommonLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule, 
+    MatListModule,    
+    MatToolbarModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
